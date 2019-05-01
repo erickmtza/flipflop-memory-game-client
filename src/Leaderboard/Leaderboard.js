e4f3d42bee5f402d4dd6b3278f011f5a4aef8451
Erick Martinez <erick.martinez2013@gmail.com>
2019-05-01 18:13:28 -0500

    New static component for the leaderboard component

---
+++ src/Leaderboard/Leaderboard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Leaderboard() {
+    return (
+        <main>
+            <h2>Top 15 Leaderboard</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Place</th>
+                        <th>Player</th>
+                        <th>Time (sec.)</th>
+                        <th>Date</th>
+                    </tr>
+                </tbody>
+                
+            </table>
+            <blockquote cite="">"Scientific research has shown that the human brain starts remembering things from the womb. In fact, memory begins to work 20 weeks after conception."
+            </blockquote>
+        </main>
+       
+    )
+}
+
+export default Leaderboard

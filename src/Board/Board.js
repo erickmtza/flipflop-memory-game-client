42ae2474d04dfe7bea72a596560cd47adb3e3616
Erick Martinez <erick.martinez2013@gmail.com>
2019-05-03 14:49:46 -0500

    Set up all cards through Board component

---
+++ src/Board/Board.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Card from '../Card/Card'
+
+import './Board.css'
+
+export default function Board({disabled, dimension, cards, solved, flipped, handleClick}) {
+    return (
+        <div className='board '>
+            {cards.map((card) => (
+                <Card
+                    key={card.id}
+                    id={card.id}
+                    type={card.type}
+                    width={dimension < 700 ? dimension / 4.25 : 700 / 4.5}
+                    height={dimension < 700 ? dimension / 4.5 : 700 / 4.5}
+                    flipped={flipped.includes(card.id)}
+                    handleClick={handleClick}
+                    solved={solved.includes(card.id)}
+                    disabled={disabled || solved.includes(card.id)}
+                />
+            ))}
+        </div>
+    )
+
+}

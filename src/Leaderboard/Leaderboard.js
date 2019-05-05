d0e2c577441ef8d57b074be6d023740c78a9cb86
Erick Martinez <erick.martinez2013@gmail.com>
2019-05-04 21:47:59 -0500

    Mapping though array to create player table

--- src/Leaderboard/Leaderboard.js
+++ src/Leaderboard/Leaderboard.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import MemoryContext from '../MemoryContext/MemoryContext'
 
 function Leaderboard() {
+    const value = useContext(MemoryContext)
+
+    const players = value.players.map((player, i) => (
+        <tr key={i}>
+            <td>{player.player_id}</td>
+            <td>{player.player_name}</td>
+            <td>{player.timer}</td>
+            <td>{player.date_published}</td>
+        </tr>
+    ))
+
     return (
         <main>
-            <h2>Top 15 Leaderboard</h2>
+            <h2>Top {value.players.length} Leaderboard</h2>
             <table>
                 <tbody>
                     <tr>
@@ -12,6 +24,7 @@ function Leaderboard() {
                         <th>Time (sec.)</th>
                         <th>Date</th>
                     </tr>
+                    {players}
                 </tbody>
                 
             </table>
